9f224909549f05cee7e5cb5874f6ffd044584f15
AndreySoKuR <73405802+AndreySoKuR@users.noreply.github.com>
2022-07-20 11:49:26 +0300

    Add files via upload

---
+++ vue_todo-master/src/components/common/DatePicker.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="calendarBox">
+    <input id="calendar" placeholder="클릭하면 달력이 보입니다." >
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    $("#calendar").datepicker();
+  }
+}
+</script>
+
+<style lang="css" scoped>
+  #calendar {
+    float: right;
+    margin: 10px;
+    font-size: 0.9rem;
+    text-align: right;
+  }
+</style>
